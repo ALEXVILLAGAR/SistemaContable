9f2c57547421376e72c558de6c15593fcc7d289c
pipe542 <pipegomes44@gmail.com>
2020-09-09 22:12:45 -0500

    modal home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,9 +15,16 @@
                     @endif
 
                     Bienvenido :  ( {{$user-> name}} )
+
+                    <!-- Large modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
                 </div>
+
             </div>
         </div>
     </div>
 </div>
+@include('modal.ModalCrearProducto')
 @endsection
+
